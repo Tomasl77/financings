72c395a65ca3b6dcc2feabe85374b9495522e1ba
Tomas LOBGEOIS <lobgeois.tomas@free.fr>
2019-09-03 17:25:48 +0200

    Client controller

--- financings/src/main/java/fr/formation/financings/controllers/ClientController.java
+++ financings/src/main/java/fr/formation/financings/controllers/ClientController.java
@@ -2,9 +2,11 @@ package fr.formation.financings.controllers;
 
 import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +29,15 @@ public class ClientController {
     @GetMapping("/{id}")
     protected Client getOne(@PathVariable("id") Long id) {
 	return service.getOne(id);
+    }
+
+    @DeleteMapping("/{id}")
+    protected void deleteOne(@PathVariable("id") Long id) {
+	service.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    protected void updateOne(@RequestBody @PathVariable("id") Long id) {
+	// Do the put client
     }
 }
