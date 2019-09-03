595d9de990ab7213b70496a0edf7fcbc5875c8c8
Tomas LOBGEOIS <lobgeois.tomas@free.fr>
2019-09-03 17:05:15 +0200

    change into inter

--- financings/src/main/java/fr/formation/financings/services/ClientService.java
+++ financings/src/main/java/fr/formation/financings/services/ClientService.java
@@ -3,7 +3,7 @@ package fr.formation.financings.services;
 import fr.formation.financings.dtos.ClientDto;
 import fr.formation.financings.entities.Client;
 
-public class ClientService {
+public interface ClientService {
 
     void create(ClientDto dto);
 
