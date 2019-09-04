ca2e05b920320e0ef5f4a4c4a567b18cdc4bddde
Tomas LOBGEOIS <lobgeois.tomas@free.fr>
2019-09-04 10:19:19 +0200

    impl service

---
+++ financings/src/main/java/fr/formation/financings/repositories/ContactRepository.java
@@ -0,0 +1,9 @@
+package fr.formation.financings.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.formation.financings.entities.Contact;
+
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+    //
+}
